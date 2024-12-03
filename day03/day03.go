afe3a82b3f80aeeff79ce70a1448e8c666adca14
Gjum <code.gjum@gmail.com>
2024-12-03 17:36:17 +0100

    day 03 part 2

--- day03/day03.go
+++ day03/day03.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func part1(memb []byte) int {
@@ -19,6 +20,28 @@ func part1(memb []byte) int {
 	return sum
 }
 
+func part2(memb []byte) int {
+	memory := string(memb[:])
+	sum := 0
+	re := regexp.MustCompile(`do(n't)?\(\)|mul\(([0-9]+),([0-9]+)\)`)
+	enabled := true
+	for _, match := range re.FindAllStringSubmatch(memory, -1) {
+		switch strings.Split(match[0], "(")[0] {
+		case "do":
+			enabled = true
+		case "don't":
+			enabled = false
+		case "mul":
+			if enabled {
+				a, _ := strconv.Atoi(match[2])
+				b, _ := strconv.Atoi(match[3])
+				sum += a * b
+			}
+		}
+	}
+	return sum
+}
+
 func main() {
 	memory, err := os.ReadFile(os.Args[1])
 	if err != nil {
@@ -28,4 +51,7 @@ func main() {
 
 	sum := part1(memory)
 	fmt.Println("p1:", sum)
+
+	sum2 := part2(memory)
+	fmt.Println("p2:", sum2)
 }
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -15,3 +15,14 @@ func TestDay03p1(t *testing.T) {
 		t.Fatalf(`sum = %d, expected 161`, sum)
 	}
 }
+
+func TestDay03p2(t *testing.T) {
+	memory, err := os.ReadFile("example2.in")
+	if err != nil {
+		t.Fatal(err)
+	}
+	sum := part2(memory)
+	if sum != 48 {
+		t.Fatalf(`sum = %d, expected 48`, sum)
+	}
+}
